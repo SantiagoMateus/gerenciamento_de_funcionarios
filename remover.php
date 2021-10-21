24807512323663f51d4422c53acb1f66a8a02108
santiagojera <mateussantiago@jera.com.br>
2021-10-21 10:12:37 -0400

    remove files

--- remover.php
+++
@@ -1,9 +0,0 @@
-<?php
-   
-    include "conexao.php";
- 
-    //Recuperamos o id enviado pelo método get, na página de cadastro.
-    remover_funcionario($conexao, $_GET['id']);
-    
-    //Retorna para a página incial
-    header('Location: funcionarios.php');
